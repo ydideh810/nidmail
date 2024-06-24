71a787f6b62bbd05eb73087fdfcf3bb5695c5abe
ydideh810 <yd.baller810@gmail.com>
2024-06-24 13:03:15 -0400

    Update Layout.jsx

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -6,8 +6,8 @@ const Layout = ({ children }) => {
     return (
         <>
             <Head>
-                <title>Mailgo</title>
-                <meta name='description' content="Gain control of your business's growth with Mailgo's comprehensive marketing, automation, and email marketing platform." />
+                <title>Niddam</title>
+                <meta name='description' content="Experience the power of AI without sacrificing your privacy." />
                 <meta name='viewport' content='width=device-width, initial-scale=1' />
                 <link rel='icon' href='/favicon.ico' />
             </Head>
