f30849003551f58d5b3b3f89d4b839bded281ce8
ydideh810 <yd.baller810@gmail.com>
2024-06-24 10:48:29 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,7 @@
-import CTA from "@/components/ui/CTA";
 import FAQs from "@/components/ui/FAQs";
 import Features from "@/components/ui/Features";
 import Hero from "@/components/ui/Hero";
 import Pricing from "@/components/ui/Pricing";
-import Testimonial from "@/components/ui/Testimonial";
 import VisualFeatures from "@/components/ui/VisualFeatures";
 
 export default function Home() {
@@ -12,8 +10,6 @@ export default function Home() {
       <Hero />
       <VisualFeatures />
       <Features />
-      <CTA />
-      <Testimonial />
       <Pricing />
       <FAQs />
     </>
