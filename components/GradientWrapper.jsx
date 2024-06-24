81d28b2de997d0d27ed98d39e24732cf13bed7d0
ydideh810 <yd.baller810@gmail.com>
2024-06-24 13:01:27 -0400

    Update GradientWrapper.jsx

--- components/GradientWrapper.jsx
+++ components/GradientWrapper.jsx
@@ -5,7 +5,7 @@ const GradientWrapper = ({ children, ...props }) => (
         <div className={`absolute m-auto blur-[160px] ${props.wrapperClassName || ""}`}
             style={{
                 background:
-                    "linear-gradient(180deg, #7C3AED 0%, rgba(152, 103, 240, 0.984375) 0.01%, rgba(237, 78, 80, 0.2) 100%)",
+                    "linear-gradient(180deg, #B20000 0%, rgba(152, 103, 240, 0.984375) 0.01%, rgba(237, 78, 80, 0.2) 100%)",
             }}>
 
         </div>
