5b6398e0b7065eadc49eaadf9ab1b9b366974cc6
ydideh810 <yd.baller810@gmail.com>
2024-06-24 10:56:48 -0400

    Update Navbar.jsx

--- components/ui/Navbar/Navbar.jsx
+++ components/ui/Navbar/Navbar.jsx
@@ -11,7 +11,6 @@ const Navbar = () => {
     const navigation = [
         { name: "Features", href: "/#features" },
         { name: "Pricing", href: "/#pricing" },
-        { name: "Testimonials", href: "/#testimonials" },
         { name: "FAQs", href: "/#faqs" },
     ]
 
@@ -45,11 +44,9 @@ const Navbar = () => {
                             }
                         </ul>
                         <div className="gap-x-6 items-center justify-end mt-6 space-y-6 md:flex md:space-y-0 md:mt-0">
-                            <Link href="/login" className="block hover:text-gray-50">
-                                Sign in
-                            </Link>
+                        
                             <NavLink href="/pricing" className="flex items-center justify-center gap-x-1 text-sm text-white font-medium custom-btn-bg border border-gray-500 active:bg-gray-900 md:inline-flex">
-                                Start now
+                                Explore
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
                                     <path fillRule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clipRule="evenodd" />
                                 </svg>
