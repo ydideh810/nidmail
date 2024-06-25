0dba8616293ba953da8e9f772e222a98016f6873
ydideh810 <yd.baller810@gmail.com>
2024-06-25 00:22:22 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import FAQs from "@/components/ui/FAQs";
 import Features from "@/components/ui/Features";
 import Hero from "@/components/ui/Hero";
 import Pricing from "@/components/ui/Pricing";
-import VisualFeatures from "@/components/ui/VisualFeatures";
+
 
 export default function Home() {
   return (
