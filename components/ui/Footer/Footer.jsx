290d1cacee9ec0167f1bee2a5f35da793f2de1e6
ydideh810 <yd.baller810@gmail.com>
2024-06-30 22:36:14 -0400

    Update Footer.jsx

--- components/ui/Footer/Footer.jsx
+++ components/ui/Footer/Footer.jsx
@@ -5,14 +5,14 @@ const Footer = () => {
             <div className="custom-screen">
                 <div className="mt-10 py-8 border-t border-gray-800 items-center justify-between sm:flex">
                     <p className="text-gray-400 text-center">© 2024 Niddam Inc. All rights reserved.</p>
-                    <div className="flex items-center justify-center gap-x-6 text-gray-500 mt-6 sm:mt-0">
+                    <div className="flex items-center justify-center gap-x-6 text-gray-50 mt-6 sm:mt-0">
                         <a href="https://x.com/niddam_ai" target="_blank" aria-label="social media">
                             <svg className="w-6 h-6 hover:text-gray-200 duration-150" fill="none" viewBox="0 0 48 48"><g clip-path="url(#clip0_17_80)"><path fill="currentColor" d="M15.1 43.5c18.11 0 28.017-15.006 28.017-28.016 0-.422-.01-.853-.029-1.275A19.998 19.998 0 0048 9.11c-1.795.798-3.7 1.32-5.652 1.546a9.9 9.9 0 004.33-5.445 19.794 19.794 0 01-6.251 2.39 9.86 9.86 0 00-16.788 8.979A27.97 27.97 0 013.346 6.299 9.859 9.859 0 006.393 19.44a9.86 9.86 0 01-4.462-1.228v.122a9.844 9.844 0 007.901 9.656 9.788 9.788 0 01-4.442.169 9.867 9.867 0 009.195 6.843A19.75 19.75 0 010 39.078 27.937 27.937 0 0015.1 43.5z" /></g><defs><clipPath id="clip0_17_80"><path fill="currentColor" d="M0 0h48v48H0z" /></clipPath></defs></svg>
                         </a>
                         <a href="https://www.linkedin.com/in/yonis-dideh-33a904181/" target="_blank" aria-label="social media">
                             <svg className="w-6 h-6 hover:text-gray-200 duration-150" fill="none" viewBox="0 0 48 48"><g clip-path="url(#clip0_17_68)"><path fill="currentColor" d="M44.447 0H3.544C1.584 0 0 1.547 0 3.46V44.53C0 46.444 1.584 48 3.544 48h40.903C46.407 48 48 46.444 48 44.54V3.46C48 1.546 46.406 0 44.447 0zM14.24 40.903H7.116V17.991h7.125v22.912zM10.678 14.87a4.127 4.127 0 01-4.134-4.125 4.127 4.127 0 014.134-4.125 4.125 4.125 0 010 8.25zm30.225 26.034h-7.115V29.766c0-2.653-.047-6.075-3.704-6.075-3.703 0-4.265 2.896-4.265 5.887v11.325h-7.107V17.991h6.826v3.13h.093c.947-1.8 3.272-3.702 6.731-3.702 7.21 0 8.541 4.744 8.541 10.912v12.572z" /></g><defs><clipPath id="clip0_17_68"><path fill="currentColor" d="M0 0h48v48H0z" /></clipPath></defs></svg>
                         </a>
-                        <a href="https://www.producthunt.com/posts/niddam?embed=true&utm_source=badge-featured&utm_medium=badge&utm_souce=badge-niddam" target="_blank"><img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=466294&theme=light" alt="Niddam - Your&#0032;Ultimate&#0032;Private&#0032;Assistant | Product Hunt" style="width: 250px; height: 54px;" width="250" height="54" /></a>
+                        
                     </div>
                 </div>
             </div>
